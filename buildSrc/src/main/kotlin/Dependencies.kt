930989d4f4a096e23ba4727c80d08dc9e8f671f7
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2020-03-09 13:52:58 +0100

    added simple sample activity added version and config kotlin dsl files

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,19 @@
+object Versions {
+
+    const val kotlin = "1.3.61"
+    const val buildTools = "3.6.1"
+
+    const val appCompat = "1.1.0"
+    const val ktx = "1.2.0"
+    const val material = "1.1.0"
+    const val constraintLayout = "1.1.3"
+    const val navigation = "2.2.1"
+
+    const val jUnit = "4.12"
+    const val jUnitAndroidX = "1.1.1"
+    const val espresso = "3.2.0"
+}
+
+object Config {
+
+}
