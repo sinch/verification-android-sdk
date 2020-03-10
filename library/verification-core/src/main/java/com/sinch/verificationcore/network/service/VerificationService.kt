f15a8f2d7a5f88714ef473db0be9b789e126f005
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2020-03-10 11:01:27 +0100

    test

--- library/verification-core/src/main/java/com/sinch/verificationcore/network/service/VerificationService.kt
+++ library/verification-core/src/main/java/com/sinch/verificationcore/network/service/VerificationService.kt
@@ -1,7 +1,14 @@
 package com.sinch.verificationcore.network.service
 
+import com.sinch.verificationcore.model.verification.VerificationRequestData
+import com.sinch.verificationcore.model.verification.VerificationRequestResponse
+import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface VerificationService {
+
+    @POST("verifications")
+    fun createVerificationRequest(@Body data: VerificationRequestData): Call<VerificationRequestResponse>
 
 }
