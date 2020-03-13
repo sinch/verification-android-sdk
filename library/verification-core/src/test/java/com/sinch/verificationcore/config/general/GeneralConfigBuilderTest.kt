9d5fb7d85f9056a6b4cd8aa8513f92884ebf69c3
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2020-03-13 14:44:58 +0100

    reformat

--- library/verification-core/src/test/java/com/sinch/verificationcore/config/general/GeneralConfigBuilderTest.kt
+++ library/verification-core/src/test/java/com/sinch/verificationcore/config/general/GeneralConfigBuilderTest.kt
@@ -3,18 +3,16 @@ package com.sinch.verificationcore.config.general
 import android.os.Build
 import androidx.test.core.app.ApplicationProvider
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
-import com.sinch.verificationcore.auth.AppKeyAuthorizationMethod
-import com.sinch.verificationcore.auth.AuthorizationMethod
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import org.junit.Assert.*
 
 
 @RunWith(
-    RobolectricTestRunner::class)
+    RobolectricTestRunner::class
+)
 @Config(sdk = [Build.VERSION_CODES.O_MR1])
 class GeneralConfigBuilderTest {
 
@@ -26,7 +24,7 @@ class GeneralConfigBuilderTest {
             .authMethod(mock())
             .apiHost(baseUrl).build()
 
-         assertEquals(config.retrofit.baseUrl().toString(), baseUrl)
+        assertEquals(config.retrofit.baseUrl().toString(), baseUrl)
     }
 
 }
