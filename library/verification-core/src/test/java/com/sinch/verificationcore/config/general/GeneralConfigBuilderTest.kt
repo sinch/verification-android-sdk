3c09c568a8034a6f42b4fa9df812bdca216a7b7c
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2020-03-17 07:32:03 +0100

    base config tests updated

--- library/verification-core/src/test/java/com/sinch/verificationcore/config/general/GeneralConfigBuilderTest.kt
+++ library/verification-core/src/test/java/com/sinch/verificationcore/config/general/GeneralConfigBuilderTest.kt
@@ -3,7 +3,7 @@ package com.sinch.verificationcore.config.general
 import android.os.Build
 import androidx.test.core.app.ApplicationProvider
 import com.nhaarman.mockitokotlin2.mock
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -18,13 +18,13 @@ class GeneralConfigBuilderTest {
 
     @Test
     fun shouldBaseUrlMatch() {
-        val baseUrl = "http://localhost.com/"
+        val apiHost = "http://localhost.com/"
         val config = SinchGeneralConfig.Builder()
             .context(ApplicationProvider.getApplicationContext())
             .authMethod(mock())
-            .apiHost(baseUrl).build()
-
-        assertEquals(config.retrofit.baseUrl().toString(), baseUrl)
+            .apiHost(apiHost).build()
+        
+        assertTrue(config.retrofit.baseUrl().toString().startsWith(apiHost))
     }
 
 }
